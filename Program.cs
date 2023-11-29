58cb06039b83a7b72363efd829e51881595597cf
Luís Felipe <luisrodrigues@bne.com.br>
2023-11-29 18:47:01 -0300

    Criação de uma tag

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blog.Data;
+using Blog.Models;
+
+using var context = new BlogDataContext();
+
+Console.WriteLine("| TAGS |");
+foreach (var tag in context.Tags)
+{
+    Console.WriteLine($"- Id: {tag.Id} \n" +
+                      $"- Nome: {tag.Name} \n" +
+                      $"- Url: {tag.Slug}");
+
+    Console.WriteLine();
+}
