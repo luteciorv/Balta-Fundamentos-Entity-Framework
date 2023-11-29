9095a005cb301feab50e13125c7f25529a133a74
Luís Felipe <luisrodrigues@bne.com.br>
2023-11-29 18:40:58 -0300

    Configuração do DataContext

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -5,5 +5,14 @@ namespace Blog.Data;
 
 public class BlogDataContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=Teste@123;Trusted_Connection=False;TrustServerCertificate=True;");
+
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<PostTag> PostTags { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
 }
